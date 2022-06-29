c81732d2a9725ad577d8b14afa3c85720c7eb25b
Evgeniy <36328422+targetss@users.noreply.github.com>
2022-06-29 17:42:36 +0500

    Update main.cpp

--- static in class/main.cpp
+++ static in class/main.cpp
@@ -3,11 +3,11 @@
 class Book
 {
 public:
-	Book(const char*, const char*, const char*, int);
-	Book(int, char*, char*, char*, int);
+	Book(int, const char*, const char*, const char*, int);
 	Book(const Book&);
 
 	int GetID();
+	void SetID(int);
 
 	~Book();
 private:
@@ -16,34 +16,15 @@ private:
 	char* author;
 	char* genre;
 	int count;
-	static int IDNumber;
 };
 
-int Book::IDNumber = 0;
-
-Book::Book(const char* title_, const char* author_, const char* genre_, int count_)
+Book::Book(int ID_, const char* title_, const char* author_, const char* genre_, int count_)
 {
-	this->ID = IDNumber;
+	//IDNumber++;
+
 	if (title == nullptr || author == nullptr || genre == nullptr)
 	{
-		delete title, author, genre;
-		title = new char[strlen(title_) + 1];
-		author = new char[strlen(author_) + 1];
-		genre = new char[strlen(genre_) + 1];
-	}
-
-	strcpy_s(title, strlen(title_)+1, title_);
-	strcpy_s(author, strlen(author_) + 1, author_);
-	strcpy_s(genre, strlen(genre_) + 1, genre_);
-	this->count = count_;
-	IDNumber++;
-}
-
-Book::Book(int ID_, char* title_, char* author_, char* genre_, int count_)
-{
-	if (title == nullptr || author == nullptr || genre == nullptr)
-	{
-		delete title, author, genre;
+		//delete title, author, genre;
 		title = new char[strlen(title_) + 1];
 		author = new char[strlen(author_) + 1];
 		genre = new char[strlen(genre_) + 1];
@@ -58,12 +39,29 @@ Book::Book(int ID_, char* title_, char* author_, char* genre_, int count_)
 
 Book::Book(const Book& other)
 {
+	if (this->title != nullptr || this->author != nullptr || this->genre != nullptr)
+	{
+		delete title, author, genre;
+		title = new char[strlen(other.title) + 1];
+		author = new char[strlen(other.author) + 1];
+		genre = new char[strlen(other.genre) + 1];
+	}
+	strcpy_s(this->title, strlen(other.title) + 1, other.title);
+	strcpy_s(this->author, strlen(other.author) + 1, other.author);
+	strcpy_s(this->genre, strlen(other.genre) + 1, other.genre);
+	this->ID = other.ID;
+	this->count = other.count;
 
 }
 
 int Book::GetID()
 {
 	return ID;
+}
+
+void Book::SetID(int num)
+{
+	this->ID = num;
 }
 
 Book::~Book()
@@ -79,8 +77,8 @@ int main(int argc, char* argv[])
 	const char *ame = "hello";
 	std::cout << strlen(ame) << std::endl;
 
-	Book b1("name", "author", "genre", 2);
-	Book b2("name1", "author1", "genre1", 1);
+	Book b1(0, "name", "author", "genre", 2);
+	Book b2(1, "name1", "author1", "genre1", 1);
 
 	std::cout << b1.GetID() << std::endl;
 	std::cout << b2.GetID() << std::endl;
